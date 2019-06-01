bb342202d1841577e35481fb073d97c589626e0d
Jason Park <parkjs814@gmail.com>
2019-06-02 03:52:05 +0900

    Things

--- src/components/Learn/index.js
+++ src/components/Learn/index.js
@@ -6,21 +6,22 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons/faMicrophone';
 import Section from '../Section/Section';
+import SpeechRecognition from 'react-speech-recognition';
 
-function Learn({ className }) {
+function Learn({ className, transcript, startListening, stopListening }) {
   const [attempting, setAttempting] = useState(false);
 
   return (
     <div className={classes('Learn', className)}>
       <div className={classes('sectionContainer', attempting && 'attempting')}>
-        <Section/>
+        <Section word="You need to go straight."/>
         {
           attempting &&
           <div className="divider"/>
         }
         {
           attempting &&
-          <Section/>
+          <Section word={transcript}/>
         }
       </div>
       <div className="icon prev">
@@ -29,11 +30,17 @@ function Learn({ className }) {
       <div className="icon next">
         <FontAwesomeIcon icon={faChevronRight} fixedWidth/>
       </div>
-      <div className="icon mic" onClick={() => setAttempting(!attempting)}>
+      <div className="icon mic" onClick={() => {
+        setAttempting(!attempting);
+        if (!attempting) {
+          startListening();
+          window.setTimeout(() => stopListening(), 3000);
+        }
+      }}>
         <FontAwesomeIcon icon={faMicrophone} fixedWidth/>
       </div>
     </div>
   );
 }
 
-export default Learn;
+export default SpeechRecognition({ autoStart: false })(Learn);
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './stylesheet.scss';
 import { classes } from '../../common/utils';
-import sampleVideo from '../../assets/speech/can_you_turn_left_here.mov';
+import sampleVideo from '../../assets/speech/can_you_turn_left_here.mp4';
 
-function Section({ className }) {
+function Section({ className, word }) {
   return (
     <div className={classes('Section', className)}>
-      <span className="word">
-        You need to go straight.
-      </span>
+      <span className="word">{word}</span>
       <span className="pronunciation">
           You need to go straight.
         </span>
       <video width="480" height="360" controls>
-        <source src={sampleVideo} type="video/mov"/>
+        <source src={sampleVideo} type="video/mp4"/>
         Your browser does not support the video tag.
       </video>
       <div>
